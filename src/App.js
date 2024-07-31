be7bf4455a3640b21b24459dd363c533a7fc1d43
ChethanL07 <144891137+ChethanL07@users.noreply.github.com>
2024-07-31 18:40:57 +0530

    -

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import AdminDashboard from './components/AdminDashboard';
 import StudentDashboard from './components/StudentDashboard';
